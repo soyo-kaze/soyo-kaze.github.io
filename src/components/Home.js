fba64ca00278cd47950ecefc4f53b0c3756f073c
soyo-kaze <07sameersaini@gmail.com>
2021-05-03 21:31:57 +0530

    some more code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import "../styles/Home.css";
 import dp from '../assets/pic.png';
-import {Row,Col} from 'react-bootstrap';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import bmc from '../assets/bmc.png';
+import twitch from '../assets/twitch.webp';
+import github from '../assets/github.png';
+import youtube from '../assets/youtube.png';
+import linkedin from '../assets/linkedin.png';
+import twitter from '../assets/twitter.png';
+import medium from '../assets/Medium.png';
+import Tabs from './Tabs';
+//import {Row,Col} from 'react-bootstrap';
+//import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () =>{
+	const linksArray = [
+		{
+			img:twitch,
+			link:"https://twitch.tv/kazetech",
+			tag:"Twitch"
+		},
+		{
+			img:twitter,
+			link:"https://twitter.com/soyokaz3",
+			tag:"Twitter"
+		},
+		{
+			img:linkedin,
+			link:"https://www.linkedin.com/in/sameer-aka-soyokaze/",
+			tag:"LinkedIn"
+		},
+		{
+			img:youtube,
+			link:"https://youtube.com/c/smgenz",
+			tag:"Youtube"
+		},
+		{
+			img:github,
+			link:"https://github.com/soyo-kaze",
+			tag:"GitHub"
+		},
+		{
+			img:medium,
+			link:"https://medium.com/@soyokaze",
+			tag:"Medium"
+		}
+		
+	]
 	return (
 		<>
 			<div className="home__main">
@@ -13,6 +54,9 @@ const Home = () =>{
 							<img src={dp} alt="my_image"/>
 						</span>
 						<p>Sameer Saini</p>
+					</span>
+					<span className="my__info tabs">
+						{linksArray.map(({img,link,tag}) => <Tabs image={img} link={link} tag={tag}/>)}
 					</span>
 			</div>
 		</>
